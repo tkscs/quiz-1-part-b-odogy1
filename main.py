1ae8e431f976e76ffd600aecee858eff61ea7172
odogy1 <ocharrette2028@kehillahstudent.org>
2025-10-24 11:29:14 -0700

    Coding Quiz

--- main.py
+++ main.py
@@ -1,5 +1,15 @@
 import turtle
 
-# YOUR CODE HERE
+for i in range(4):
+    turtle.forward(100)
+    turtle.left(90)
+
+turtle.forward(50)
+turtle.left(45)
+
+for i in range(4):
+    turtle.forward(71)
+    turtle.left(90)
+
 
 turtle.exitonclick()
